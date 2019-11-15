c5269a64bd31203714de9166634510ee550a26bc
herschel666 <kraemerseele@gmx.de>
2019-11-15 18:46:22 +0100

    feat: add fullscreen toggle

--- theme.js
+++ theme.js
@@ -1,8 +1,51 @@
+import { Fragment, createElement as e, useRef } from 'react';
 import merge from 'lodash.merge';
+import styled from '@emotion/styled';
 import { yellow } from 'mdx-deck/themes';
 
 const textTransformNone = {
   textTransform: 'none',
+};
+const handleFullscreen = (isFullsscreen) => {
+  if (isFullsscreen) {
+    return document.exitFullscreen();
+  }
+  return document.body.requestFullscreen();
+};
+const Button = styled.button`
+  position: fixed;
+  top: 0.5rem;
+  left: 0.5rem;
+  opacity: 0;
+  transition: opacity 200ms ease-in-out;
+  z-index: 100;
+  &:hover,
+  &:focus {
+    opacity: 1;
+  }
+`;
+
+const Provider = ({ children }) => {
+  const isFullsscreen = useRef(false);
+  const label = isFullsscreen.current ? 'Leave fullscreen' : 'Enter fullscreen';
+  const onClick = () => {
+    if (isFullsscreen.current === null) {
+      return;
+    }
+    const prevState = isFullsscreen.current;
+    isFullsscreen.current = null;
+    handleFullscreen(isFullsscreen.current).then(
+      () => {
+        isFullsscreen.current = !prevState;
+      },
+      () => {
+        isFullsscreen.current = prevState;
+      }
+    );
+  };
+  const onMouseUp = (evnt) => evnt.target.blur();
+
+  return e(Fragment, null, e(Button, { onClick, onMouseUp }, label), children);
 };
 
 export default merge(yellow, {
@@ -33,4 +76,5 @@ export default merge(yellow, {
       backgroundColor: '#d1e9ff',
     },
   },
+  Provider,
 });
